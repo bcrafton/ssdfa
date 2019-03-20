87fe6342526a9e12efbdbe1fa0841d17a5082a2a
Brian A Crafton <bcrafton3@KL2125-01.ece.gatech.edu>
2019-03-20 01:12:31 -0400

    imgs and stuff

---
+++ cifar10_conv1.py
@@ -0,0 +1,315 @@
+
+import argparse
+import os
+import sys
+import matplotlib.pyplot as plt
+
+##############################################
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--epochs', type=int, default=100)
+parser.add_argument('--batch_size', type=int, default=64)
+parser.add_argument('--alpha', type=float, default=0.005)
+parser.add_argument('--l2', type=float, default=0.)
+parser.add_argument('--decay', type=float, default=1.)
+parser.add_argument('--eps', type=float, default=1.)
+parser.add_argument('--dropout', type=float, default=0.0)
+parser.add_argument('--act', type=str, default='tanh')
+parser.add_argument('--bias', type=float, default=0.)
+parser.add_argument('--gpu', type=int, default=0)
+parser.add_argument('--dfa', type=int, default=0)
+parser.add_argument('--fa', type=int, default=0)
+parser.add_argument('--sparse', type=int, default=0)
+parser.add_argument('--rank', type=int, default=0)
+parser.add_argument('--init', type=str, default="sqrt_fan_in")
+parser.add_argument('--opt', type=str, default="adam")
+parser.add_argument('--save', type=int, default=0)
+parser.add_argument('--name', type=str, default="cifar10_conv")
+parser.add_argument('--load', type=str, default=None)
+args = parser.parse_args()
+
+if args.gpu >= 0:
+    os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"]=str(args.gpu)
+
+##############################################
+
+import time
+import tensorflow as tf
+import keras
+import math
+import numpy as np
+
+from lib.Model import Model
+
+from lib.Layer import Layer 
+from lib.ConvToFullyConnected import ConvToFullyConnected
+from lib.FullyConnected import FullyConnected
+from lib.Convolution import Convolution
+from lib.MaxPool import MaxPool
+from lib.Dropout import Dropout
+from lib.FeedbackFC import FeedbackFC
+from lib.FeedbackConv import FeedbackConv
+
+from lib.Activation import Activation
+from lib.Activation import Sigmoid
+from lib.Activation import Relu
+from lib.Activation import Tanh
+from lib.Activation import Softmax
+from lib.Activation import LeakyRelu
+from lib.Activation import Linear
+
+##############################################
+
+cifar10 = tf.keras.datasets.cifar10.load_data()
+
+##############################################
+
+def factors(x):
+    l = [] 
+    for i in range(1, x + 1):
+        if x % i == 0:
+            l.append(i)
+    
+    mid = int(len(l) / 2)
+    
+    if (len(l) % 2 == 1):
+        return [l[mid], l[mid]]
+    else:
+        return l[mid-1:mid+1]
+
+
+def viz(name, filters):
+    fh, fw, fin, fout = np.shape(filters)
+    filters = filters.T
+    assert(np.shape(filters) == (fout, fin, fw, fh))
+    [nrows, ncols] = factors(fin * fout)
+    filters = np.reshape(filters, (nrows, ncols, fw, fh))
+
+    for ii in range(nrows):
+        for jj in range(ncols):
+            if jj == 0:
+                row = filters[ii][jj]
+            else:
+                row = np.concatenate((row, filters[ii][jj]), axis=1)
+                
+        if ii == 0:
+            img = row
+        else:
+            img = np.concatenate((img, row), axis=0)
+            
+    plt.imsave(name, img, cmap="gray")
+
+
+EPOCHS = args.epochs
+TRAIN_EXAMPLES = 50000
+TEST_EXAMPLES = 10000
+BATCH_SIZE = args.batch_size
+
+if args.act == 'tanh':
+    act = Tanh()
+elif args.act == 'relu':
+    act = Relu()
+else:
+    assert(False)
+
+train_fc=True
+train_conv=True
+weights_fc='./transfer/cifar10_conv_weights.npy'
+weights_conv='./transfer/cifar10_conv_weights.npy'
+
+##############################################
+
+tf.set_random_seed(0)
+tf.reset_default_graph()
+
+batch_size = tf.placeholder(tf.int32, shape=())
+dropout_rate = tf.placeholder(tf.float32, shape=())
+learning_rate = tf.placeholder(tf.float32, shape=())
+X = tf.placeholder(tf.float32, [None, 32, 32, 3])
+X = tf.map_fn(lambda frame: tf.image.per_image_standardization(frame), X)
+Y = tf.placeholder(tf.float32, [None, 10])
+
+l0 = Convolution(input_sizes=[batch_size, 32, 32, 3], filter_sizes=[5, 5, 3, 96], num_classes=10, init_filters=args.init, strides=[1, 1, 1, 1], padding="SAME", alpha=learning_rate, activation=act, bias=args.bias, last_layer=False, name='conv1', load=weights_conv, train=train_conv, fa=args.fa)
+l1 = MaxPool(size=[batch_size, 32, 32, 96], ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="SAME")
+
+l3 = Convolution(input_sizes=[batch_size, 16, 16, 96], filter_sizes=[5, 5, 96, 128], num_classes=10, init_filters=args.init, strides=[1, 1, 1, 1], padding="SAME", alpha=learning_rate, activation=act, bias=args.bias, last_layer=False, name='conv2', load=weights_conv, train=train_conv, fa=args.fa)
+l4 = MaxPool(size=[batch_size, 16, 16, 128], ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="SAME")
+
+l6 = Convolution(input_sizes=[batch_size, 8, 8, 128], filter_sizes=[5, 5, 128, 256], num_classes=10, init_filters=args.init, strides=[1, 1, 1, 1], padding="SAME", alpha=learning_rate, activation=act, bias=args.bias, last_layer=False, name='conv3', load=weights_conv, train=train_conv, fa=args.fa)
+l7 = MaxPool(size=[batch_size, 8, 8, 256], ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="SAME")
+
+l9 = ConvToFullyConnected(shape=[4, 4, 256])
+
+l10 = FullyConnected(size=[4*4*256, 2048], num_classes=10, init_weights=args.init, alpha=learning_rate, activation=act, bias=args.bias, last_layer=False, name='fc1', load=weights_fc, train=train_fc, fa=args.fa)
+l11 = Dropout(rate=dropout_rate)
+
+l13 = FullyConnected(size=[2048, 2048], num_classes=10, init_weights=args.init, alpha=learning_rate, activation=act, bias=args.bias, last_layer=False, name='fc2', load=weights_fc, train=train_fc, fa=args.fa)
+l14 = Dropout(rate=dropout_rate)
+
+l16 = FullyConnected(size=[2048, 10], num_classes=10, init_weights=args.init, alpha=learning_rate, activation=Linear(), bias=args.bias, last_layer=True, name='fc3', load=weights_fc, train=train_fc, fa=args.fa)
+
+##############################################
+
+# model = Model(layers=[l0, l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13, l14, l15, l16])
+model = Model(layers=[l0, l1, l3, l4, l6, l7, l9, l10, l11, l13, l14, l16])
+
+predict = model.predict(X=X)
+
+weights = model.get_weights()
+
+backward = model.backwards(X=X, Y=Y)
+
+if args.opt == "adam" or args.opt == "rms" or args.opt == "decay":
+    if args.dfa:
+        grads_and_vars = model.dfa_gvs(X=X, Y=Y)
+    else:
+        grads_and_vars = model.gvs(X=X, Y=Y)
+        
+    if args.opt == "adam":
+        train = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0.9, beta2=0.999, epsilon=args.eps).apply_gradients(grads_and_vars=grads_and_vars)
+    elif args.opt == "rms":
+        train = tf.train.RMSPropOptimizer(learning_rate=learning_rate, decay=0.99, epsilon=args.eps).apply_gradients(grads_and_vars=grads_and_vars)
+    elif args.opt == "decay":
+        train = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).apply_gradients(grads_and_vars=grads_and_vars)
+    else:
+        assert(False)
+
+else:
+    if args.dfa:
+        train = model.dfa(X=X, Y=Y)
+    else:
+        train = model.train(X=X, Y=Y)
+
+correct = tf.equal(tf.argmax(predict,1), tf.argmax(Y,1))
+total_correct = tf.reduce_sum(tf.cast(correct, tf.float32))
+
+##############################################
+
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+tf.local_variables_initializer().run()
+
+(x_train, y_train), (x_test, y_test) = cifar10
+
+x_train = x_train.reshape(TRAIN_EXAMPLES, 32, 32, 3)
+y_train = keras.utils.to_categorical(y_train, 10)
+
+x_test = x_test.reshape(TEST_EXAMPLES, 32, 32, 3)
+y_test = keras.utils.to_categorical(y_test, 10)
+
+##############################################
+
+filename = args.name + '.results'
+f = open(filename, "w")
+f.write(filename + "\n")
+f.write("total params: " + str(model.num_params()) + "\n")
+f.close()
+
+##############################################
+
+train_accs = []
+test_accs = []
+
+for ii in range(EPOCHS):
+    if args.opt == 'decay' or args.opt == 'gd':
+        decay = np.power(args.decay, ii)
+        lr = args.alpha * decay
+    else:
+        lr = args.alpha
+        
+    print (ii)
+    
+    #############################
+    
+    _count = 0
+    _total_correct = 0
+    
+    for jj in range(int(TRAIN_EXAMPLES / BATCH_SIZE)):
+        xs = x_train[jj*BATCH_SIZE:(jj+1)*BATCH_SIZE]
+        ys = y_train[jj*BATCH_SIZE:(jj+1)*BATCH_SIZE]
+        _correct, _, _gvs, (_forward, _backward) = sess.run([total_correct, train, grads_and_vars, backward], feed_dict={batch_size: BATCH_SIZE, dropout_rate: args.dropout, learning_rate: lr, X: xs, Y: ys})
+        
+        _total_correct += _correct
+        _count += BATCH_SIZE
+        
+        # 6, 8, 10
+        '''
+        if ((jj + 1) % 100 == 0):
+            for kk in range(len(gvs)):
+                print (kk, np.shape(gvs[kk]))
+        '''
+        if (jj == 0):
+            pass
+            # print (np.std(gvs[0][0]), np.std(gvs[2][0]), np.std(gvs[4][0]), np.std(gvs[6][0]), np.std(gvs[8][0]), np.std(gvs[10][0]))
+            # print (np.std(gvs[0][1]), np.std(gvs[2][1]), np.std(gvs[4][1]), np.std(gvs[6][1]), np.std(gvs[8][1]), np.std(gvs[10][1]))        
+            # print ('----------')
+
+            img = _forward[0][0, :, :, 0]
+            plt.imsave('forward_%d_1_%d.png' % (args.fa, ii), img, cmap="gray")
+
+            img = _forward[2][0, :, :, 0]
+            plt.imsave('forward_%d_2_%d.png' % (args.fa, ii), img, cmap="gray")
+
+            img = _forward[4][0, :, :, 0]
+            plt.imsave('forward_%d_3_%d.png' % (args.fa, ii), img, cmap="gray")
+
+            # backwards is in the right order actually because we do backward loop in Model.
+            img = _backward[1][0, :, :, 0]
+            plt.imsave('backward_%d_1_%d.png' % (args.fa, ii), img, cmap="gray")
+
+            img = _backward[3][0, :, :, 0]
+            plt.imsave('backward_%d_2_%d.png' % (args.fa, ii), img, cmap="gray")
+
+            img = _backward[5][0, :, :, 0]
+            plt.imsave('backward_%d_3_%d.png' % (args.fa, ii), img, cmap="gray")
+
+            # this aint working no more bc we return a gradient for fb.
+            # same is not true for gvs
+            f3 = _gvs[6][0]
+            viz('gv_%d_3_%d.png' % (args.fa, ii), f3)
+
+            f2 = _gvs[8][0]
+            viz('gv_%d_2_%d.png' % (args.fa, ii), f2)
+
+            f1 = _gvs[10][0]
+            viz('gv_%d_1_%d.png' % (args.fa, ii), f1)
+
+    train_acc = 1.0 * _total_correct / _count
+    train_accs.append(train_acc)
+
+    #############################
+
+    _count = 0
+    _total_correct = 0
+
+    for jj in range(int(TEST_EXAMPLES / BATCH_SIZE)):
+        xs = x_test[jj*BATCH_SIZE:(jj+1)*BATCH_SIZE]
+        ys = y_test[jj*BATCH_SIZE:(jj+1)*BATCH_SIZE]
+        _correct = sess.run(total_correct, feed_dict={batch_size: BATCH_SIZE, dropout_rate: 0.0, learning_rate: 0.0, X: xs, Y: ys})
+        
+        _total_correct += _correct
+        _count += BATCH_SIZE
+        
+    test_acc = 1.0 * _total_correct / _count
+    test_accs.append(test_acc)
+    
+    #############################
+            
+    print ("train acc: %f test acc: %f" % (train_acc, test_acc))
+    
+    f = open(filename, "a")
+    f.write("train acc: %f test acc: %f\n" % (train_acc, test_acc))
+    f.close()
+
+##############################################
+
+    if args.save:
+        [w] = sess.run([weights], feed_dict={})
+        w['train_acc'] = train_accs
+        w['test_acc'] = test_accs
+        np.save(args.name, w)
+    
+##############################################
+
+
