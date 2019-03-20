1b423bb881faa98cc61bc84b21a550e6887bf18b
Brian Crafton <crafton.b@husky.neu.edu>
2019-03-20 14:05:27 -0400

    plot stuff

--- transfer/plot_acc.py
+++ transfer/plot_acc.py
@@ -5,12 +5,23 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 weights = np.load('cifar10_conv_weights.npy').item()
-plt.plot(weights['test_acc'])
+x = range(0, 100, 2)
+y = np.array(weights['test_acc'])[x]
+plt.plot(x, y, label='BP', marker='^', linestyle='None')
 
 weights = np.load('cifar10_conv_weights_fa.npy').item()
-plt.plot(weights['test_acc'])
+x = range(0, 100, 2)
+y = np.array(weights['test_acc'])[x]
+plt.plot(x, y, label='FA Structured', marker='*', linestyle='None')
 
 weights = np.load('cifar10_conv_weights_random.npy').item()
-plt.plot(weights['test_acc'])
+x = range(0, 100, 2)
+y = np.array(weights['test_acc'])[x]
+plt.plot(x, y, label='FA Random', marker="v", linestyle='None')
 
+plt.rcParams['font.sans-serif'] = "Arial"
+plt.rcParams['font.family'] = "sans-serif"
+plt.rcParams['font.size'] = 10.
+
+plt.legend()
 plt.savefig('plot_acc.png')
