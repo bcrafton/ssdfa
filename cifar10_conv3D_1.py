b1ef93e4adba1d456f83c7c359743b1a378a1ad0
Brian <bcrafton3@gatech.edu>
2019-04-12 17:58:06 -0400

    shud be able to do 3d

---
+++ cifar10_conv3D_1.py
@@ -0,0 +1,172 @@
+
+import argparse
+import os
+import sys
+
+##############################################
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--epochs', type=int, default=100)
+parser.add_argument('--batch_size', type=int, default=100)
+parser.add_argument('--alpha', type=float, default=1e-4)
+parser.add_argument('--l2', type=float, default=0.)
+parser.add_argument('--decay', type=float, default=1.)
+parser.add_argument('--eps', type=float, default=1e-5)
+parser.add_argument('--dropout', type=float, default=0.5)
+parser.add_argument('--act', type=str, default='relu')
+parser.add_argument('--bias', type=float, default=0.)
+parser.add_argument('--gpu', type=int, default=0)
+parser.add_argument('--dfa', type=int, default=0)
+parser.add_argument('--sparse', type=int, default=0)
+parser.add_argument('--rank', type=int, default=0)
+parser.add_argument('--init', type=str, default="sqrt_fan_in")
+parser.add_argument('--opt', type=str, default="adam")
+parser.add_argument('--save', type=int, default=0)
+parser.add_argument('--name', type=str, default="cifar10_conv")
+parser.add_argument('--load', type=str, default=None)
+args = parser.parse_args()
+
+if args.gpu >= 0:
+    os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"]=str(args.gpu)
+
+##############################################
+
+import time
+import tensorflow as tf
+import keras
+import math
+import numpy as np
+
+from lib.Model import Model
+
+from lib.Layer import Layer 
+from lib.ConvToFullyConnected import ConvToFullyConnected
+from lib.FullyConnected import FullyConnected
+from lib.Convolution import Convolution
+from lib.Convolution3D import Convolution3D
+from lib.MaxPool import MaxPool
+from lib.Dropout import Dropout
+from lib.FeedbackFC import FeedbackFC
+from lib.FeedbackConv import FeedbackConv
+
+from lib.Activation import Activation
+from lib.Activation import Sigmoid
+from lib.Activation import Relu
+from lib.Activation import Tanh
+from lib.Activation import Softmax
+from lib.Activation import LeakyRelu
+from lib.Activation import Linear
+
+##############################################
+
+cifar10 = tf.keras.datasets.cifar10.load_data()
+
+##############################################
+
+EPOCHS = args.epochs
+TRAIN_EXAMPLES = 50000
+TEST_EXAMPLES = 10000
+BATCH_SIZE = args.batch_size
+
+if args.act == 'tanh':
+    act = Tanh()
+elif args.act == 'relu':
+    act = Relu()
+else:
+    assert(False)
+
+train_fc=True
+if args.load:
+    train_conv=False
+else:
+    train_conv=True
+
+weights_fc=None
+weights_conv=args.load
+
+##############################################
+
+tf.set_random_seed(0)
+tf.reset_default_graph()
+
+batch_size = tf.placeholder(tf.int32, shape=())
+dropout_rate = tf.placeholder(tf.float32, shape=())
+learning_rate = tf.placeholder(tf.float32, shape=())
+X = tf.placeholder(tf.float32, [None, 32, 32, 3])
+X = tf.map_fn(lambda frame: tf.image.per_image_standardization(frame), X)
+Y = tf.placeholder(tf.float32, [None, 10])
+
+l0 = Convolution3D(input_sizes=[batch_size, 32, 32, 3], filter_sizes=[5, 5, 3, 1, 96], num_classes=10, init_filters=args.init, strides=[1, 1, 1, 1], padding="SAME", alpha=learning_rate, activation=act, bias=args.bias, last_layer=False, name='conv1', load=weights_conv, train=train_conv)
+l1 = MaxPool(size=[batch_size, 32, 32, 96], ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="SAME")
+
+l2 = ConvToFullyConnected(shape=[16, 16, 96])
+
+l3 = FullyConnected(size=[16*16*96, 10], num_classes=10, init_weights=args.init, alpha=learning_rate, activation=Linear(), bias=args.bias, last_layer=True, name='fc3', load=weights_fc, train=train_fc)
+
+##############################################
+
+model = Model(layers=[l0, l1, l2, l3])
+predict = model.predict(X=X)
+grads_and_vars = model.gvs(X=X, Y=Y)
+backwards = model.backwards(X=X, Y=Y)
+
+correct = tf.equal(tf.argmax(predict,1), tf.argmax(Y,1))
+total_correct = tf.reduce_sum(tf.cast(correct, tf.float32))
+
+##############################################
+
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+tf.local_variables_initializer().run()
+
+(x_train, y_train), (x_test, y_test) = cifar10
+
+x_train = x_train.reshape(TRAIN_EXAMPLES, 32, 32, 3)
+y_train = keras.utils.to_categorical(y_train, 10)
+
+x_test = x_test.reshape(TEST_EXAMPLES, 32, 32, 3)
+y_test = keras.utils.to_categorical(y_test, 10)
+
+##############################################
+
+filename = args.name + '.results'
+f = open(filename, "w")
+f.write(filename + "\n")
+f.write("total params: " + str(model.num_params()) + "\n")
+f.close()
+
+##############################################
+
+train_accs = []
+test_accs = []
+
+for ii in range(EPOCHS):
+    if args.opt == 'decay' or args.opt == 'gd':
+        decay = np.power(args.decay, ii)
+        lr = args.alpha * decay
+    else:
+        lr = args.alpha
+        
+    print (ii)
+    
+    #############################
+
+    for jj in range(int(TEST_EXAMPLES / BATCH_SIZE)):
+        xs = x_test[jj*BATCH_SIZE:(jj+1)*BATCH_SIZE]
+        ys = y_test[jj*BATCH_SIZE:(jj+1)*BATCH_SIZE]
+        [gvs, backs] = sess.run([grads_and_vars, backwards], feed_dict={batch_size: BATCH_SIZE, dropout_rate: 0.0, learning_rate: 0.0, X: xs, Y: ys})
+
+                
+        print ('-------------')
+        
+        for gv in gvs:
+            print (np.shape(gv[0]), np.shape(gv[1]))
+
+        '''
+        for back in backs:
+            print (np.shape(back))
+        '''
+
+
+
---
+++ lib/Convolution3D.py
@@ -0,0 +1,117 @@
+
+import tensorflow as tf
+import numpy as np
+import math
+
+from lib.Layer import Layer 
+from lib.Activation import Activation
+from lib.Activation import Sigmoid
+
+class Convolution3D(Layer):
+
+    def __init__(self, input_sizes, filter_sizes, num_classes, init_filters, strides, padding, alpha, activation, bias, last_layer, name=None, load=None, train=True):
+
+        self.input_sizes = input_sizes
+        self.filter_sizes = filter_sizes
+        self.num_classes = num_classes
+        self.batch_size, self.h, self.w, self.fin = self.input_sizes
+        self.fh, self.fw, self.fc, self.fg, self.fout = self.filter_sizes
+        self.bias = tf.Variable(tf.ones(shape=self.fout) * bias)
+        self.strides = strides
+        self.padding = padding
+        self.alpha = alpha
+        self.activation = activation
+        self.last_layer = last_layer
+        self.name = name
+        self._train = train
+        
+        if load:
+            print ("Loading Weights: " + self.name)
+            weight_dict = np.load(load, encoding='latin1').item()
+            self.filters = tf.Variable(weight_dict[self.name])
+            self.bias = tf.Variable(weight_dict[self.name + '_bias'])
+        else:
+            if init_filters == "zero":
+                filters = np.zeros(shape=self.filter_sizes)
+            elif init_filters == "sqrt_fan_in":
+                sqrt_fan_in = math.sqrt(self.h*self.w*self.fin)
+                filters = np.random.uniform(low=-1.0/sqrt_fan_in, high=1.0/sqrt_fan_in, size=self.filter_sizes)
+            elif init_filters == "alexnet":
+                filters = np.random.normal(loc=0.0, scale=0.01, size=self.filter_sizes)
+            else:
+                # glorot
+                assert(False)
+                
+            self.filters = tf.Variable(filters, dtype=tf.float32)
+
+    ###################################################################
+
+    def get_weights(self):
+        return [(self.name, self.filters), (self.name + "_bias", self.bias)]
+
+    def num_params(self):
+        filter_weights_size = self.fh * self.fw * self.fin * self.fout
+        bias_weights_size = self.fout
+        return filter_weights_size + bias_weights_size
+                
+    def forward(self, X):
+        As = []
+        for ii in range(self.fg): 
+            start = ii * self.fc
+            end = (ii + 1) * self.fc
+            input_slice = slice(start, end)
+ 
+            Z = tf.nn.conv2d(X[:, :, :, input_slice], self.filters[:, :, :, ii, :], self.strides, self.padding)
+            A = self.activation.forward(Z)
+            As.append(A)
+
+        A = tf.concat(As, axis=3)
+        # A = tf.Print(A, [tf.shape(A)], message=self.name + ': ', summarize=1000)
+        return A
+        
+    def backward(self, AI, AO, DO): 
+        DO = tf.multiply(DO, self.activation.gradient(AO))
+
+        DIs = []
+        for ii in range(self.fg): 
+            start = ii * self.fout
+            end = (ii + 1) * self.fout
+            output_slice = slice(start, end)
+
+            DI = tf.nn.conv2d_backprop_input(input_sizes=self.input_sizes, filter=self.filters[:, :, :, ii, :], out_backprop=DO[:, :, :, output_slice], strides=self.strides, padding=self.padding)
+            DIs.append(DI)
+
+        DI = tf.concat(DIs, axis=3)
+        # DI = tf.Print(DI, [tf.shape(DI)], message=self.name + ': ', summarize=1000)
+        return DI
+
+    def gv(self, AI, AO, DO):
+        if not self._train:
+            return []
+    
+        DO = tf.multiply(DO, self.activation.gradient(AO))
+
+        DFs = []
+        for ii in range(self.fg): 
+            start = ii * self.fc
+            end = (ii + 1) * self.fc
+            input_slice = slice(start, end)
+
+            start = ii * self.fout
+            end = (ii + 1) * self.fout
+            output_slice = slice(start, end)
+
+            filter_sizes = (self.fh, self.fw, self.fc, self.fout)
+
+            DF = tf.nn.conv2d_backprop_filter(input=AI[:, :, :, input_slice], filter_sizes=filter_sizes, out_backprop=DO[:, :, :, output_slice], strides=self.strides, padding=self.padding)
+            DF = tf.reshape(DF, (self.fh, self.fw, self.fc, 1, self.fout))
+            DFs.append(DF)
+
+        DF = tf.concat(DFs, axis=3)
+        DF = tf.reshape(DF, (self.fh, self.fw, self.fc, self.fg, self.fout))
+        # DF = tf.Print(DF, [tf.shape(DF)], message=self.name + ': ', summarize=1000)
+        return [(DFs, self.filters)]
+
+    ################################################################### 
+        
+        
