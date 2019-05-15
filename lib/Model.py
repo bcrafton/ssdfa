696c91f64417a7f4bf05a4f50b049600b305a001
Brian Crafton <crafton.b@husky.neu.edu>
2019-05-15 16:48:01 -0400

    vizing

--- lib/Model.py
+++ lib/Model.py
@@ -257,7 +257,7 @@ class Model:
             else:
                 D[ii] = l.backward(A[ii-1], A[ii], D[ii+1])
                 
-        return D
+        return (A, D)
     
     def dfa_backwards(self, X, Y):
         A = [None] * self.num_layers
