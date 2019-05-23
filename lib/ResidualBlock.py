e7fdb04d9e8720e35f086a0772786fc3a5e8573a
Brian Crafton <crafton.b@husky.neu.edu>
2019-05-23 18:40:47 -0400

    trying just upscaling, be careful of bugs ... caught 1 that def cud have impacted acc

--- lib/ResidualBlock.py
+++ lib/ResidualBlock.py
@@ -21,10 +21,9 @@ class ResidualBlock(Layer):
         self.init = init
         self.name = name
         
-        self.block1 = ConvBlock(input_shape=self.input_shape, filter_shape=self.filter_shape_trans, init=self.init, name=self.name + '_conv_block_1')
-        self.block2 = ConvBlock(input_shape=self.input_shape, filter_shape=self.filter_shape_body,  init=self.init, name=self.name + '_conv_block_2')
-        self.block3 = ConvBlock(input_shape=self.input_shape, filter_shape=self.filter_shape_body,  init=self.init, name=self.name + '_conv_block_3')
-        self.block4 = ConvBlock(input_shape=self.input_shape, filter_shape=self.filter_shape_body,  init=self.init, name=self.name + '_conv_block_4')
+        self.block1 = ConvBlock(input_shape=self.input_shape, filter_shape=self.filter_shape_body, init=self.init, name=self.name + '_conv_block_1')
+        self.block2 = ConvBlock(input_shape=self.input_shape, filter_shape=self.filter_shape_body, init=self.init, name=self.name + '_conv_block_2')
+        self.block3 = ConvBlock(input_shape=self.input_shape, filter_shape=self.filter_shape_body, init=self.init, name=self.name + '_conv_block_3')
 
     ###################################################################
 
@@ -41,7 +40,12 @@ class ResidualBlock(Layer):
 
     def forward(self, X):
         
-        in1 = X 
+        if self.fin < self.fout:
+            in1 = tf.stack([X, X], axis=3)
+            in1 = tf.reshape(in1, (self.batch, self.h, self.w, self.fout))
+        else:
+            in1 = X
+            
         block1 = self.block1.forward(in1)
         
         in2 = block1['aout']
@@ -50,51 +54,56 @@ class ResidualBlock(Layer):
         in3 = in2 + block2['aout']
         block3 = self.block3.forward(in3)
         
-        in4 = in3 + block3['aout']
-        block4 = self.block4.forward(in4)
-
-        aout = in4 + block4['aout']
-        cache = {'block1':block1, 'block2':block2, 'block3':block3, 'block4':block4}
+        aout = in3 + block3['aout']
+        cache = {'block1':block1, 'block2':block2, 'block3':block3}
         
         return {'aout':aout, 'cache':cache}
         
     def backward(self, AI, AO, DO, cache):    
-        block1, block2, block3, block4 = cache['block1'], cache['block2'], cache['block3'], cache['block4']
+        block1, block2, block3 = cache['block1'], cache['block2'], cache['block3']
         
-        in1 = AI
+        if self.fin < self.fout:
+            in1 = tf.stack([AI, AI], axis=3)
+            in1 = tf.reshape(in1, (self.batch, self.h, self.w, self.fout))
+        else:
+            in1 = AI
+            
         in2 = block1['aout']
         in3 = in2 + block2['aout']
-        in4 = in3 + block3['aout']
 
-        dblock4 = self.block3.backward(in4, block4['aout'], DO, block4['cache'])
-        dblock3 = self.block2.backward(in3, block3['aout'], DO, block3['cache'])
+        dblock3 = self.block3.backward(in3, block3['aout'], DO, block3['cache'])
         dblock2 = self.block2.backward(in2, block2['aout'], DO, block2['cache'])
         dblock1 = self.block1.backward(in1, block1['aout'], DO, block1['cache'])
 
-        dout = dblock1['dout']
-        cache.update({'dblock1':dblock1, 'dblock2':dblock2, 'dblock3':dblock3, 'dblock4':dblock4})
+        # dout = dblock1['dout']
+        dout = tf.reshape(DO, [self.batch, self.h, self.w, self.fin, 2])
+        dout = tf.reduce_mean(dout, axis=4)
+        
+        cache.update({'dblock1':dblock1, 'dblock2':dblock2, 'dblock3':dblock3})
         
         return {'dout':dout, 'cache':cache}
         
     def gv(self, AI, AO, DO, cache):
-        block1, block2, block3, block4 = cache['block1'], cache['block2'], cache['block3'], cache['block4']
-        dblock1, dblock2, dblock3, dblock4 = cache['dblock1'], cache['dblock2'], cache['dblock3'], cache['dblock4']
+        block1, block2, block3 = cache['block1'], cache['block2'], cache['block3']
+        dblock1, dblock2, dblock3 = cache['dblock1'], cache['dblock2'], cache['dblock3']
         
-        in1 = AI
+        if self.fin < self.fout:
+            in1 = tf.stack([AI, AI], axis=3)
+            in1 = tf.reshape(in1, (self.batch, self.h, self.w, self.fout))
+        else:
+            in1 = AI
+            
         in2 = block1['aout']
         in3 = in2 + block2['aout']
-        in4 = in3 + block3['aout']
 
         dblock1 = self.block1.gv(in1, block1['aout'], DO, dblock1['cache'])
         dblock2 = self.block2.gv(in2, block2['aout'], DO, dblock2['cache'])
         dblock3 = self.block3.gv(in3, block3['aout'], DO, dblock3['cache'])
-        dblock4 = self.block4.gv(in4, block4['aout'], DO, dblock4['cache'])
         
         grads = []
         grads.extend(dblock1)
         grads.extend(dblock2)
         grads.extend(dblock3)
-        grads.extend(dblock4)
 
         return grads
         
