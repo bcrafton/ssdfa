593a29284972c410c7e2393a01a8c1cefbef83e8
Brian Crafton <crafton.b@husky.neu.edu>
2019-05-27 18:30:52 -0400

    updating model for dfa and lel

--- lib/Model.py
+++ lib/Model.py
@@ -169,27 +169,27 @@ class Model:
             if ii == 0:
                 A[ii] = l.forward(X)
             else:
-                A[ii] = l.forward(A[ii-1])
-            
-        E = tf.nn.softmax(A[self.num_layers-1]) - Y
-        N = tf.shape(A[self.num_layers-1])[0]
+                A[ii] = l.forward(A[ii-1]['aout'])
+
+        E = tf.nn.softmax(A[self.num_layers-1]['aout']) - Y
+        N = tf.shape(A[self.num_layers-1]['aout'])[0]
         N = tf.cast(N, dtype=tf.float32)
         E = E / N
             
         for ii in range(self.num_layers-1, -1, -1):
             l = self.layers[ii]
-                
+
             if (ii == self.num_layers-1):
-                D[ii] = l.dfa_backward(A[ii-1], A[ii], E, E)
-                gvs = l.dfa_gv(A[ii-1], A[ii], E, E)
+                D[ii] = l.dfa_backward(A[ii-1]['aout'], A[ii]['aout'], E, E,               A[ii]['cache'])
+                gvs =         l.dfa_gv(A[ii-1]['aout'], A[ii]['aout'], E, E,               D[ii]['cache'])
                 grads_and_vars.extend(gvs)
             elif (ii == 0):
-                D[ii] = l.dfa_backward(X, A[ii], E, D[ii+1])
-                gvs = l.dfa_gv(X, A[ii], E, D[ii+1])
+                D[ii] = l.dfa_backward(X,               A[ii]['aout'], E, D[ii+1]['dout'], A[ii]['cache'])
+                gvs =         l.dfa_gv(X,               A[ii]['aout'], E, D[ii+1]['dout'], D[ii]['cache'])
                 grads_and_vars.extend(gvs)
             else:
-                D[ii] = l.dfa_backward(A[ii-1], A[ii], E, D[ii+1])
-                gvs = l.dfa_gv(A[ii-1], A[ii], E, D[ii+1])
+                D[ii] = l.dfa_backward(A[ii-1]['aout'], A[ii]['aout'], E, D[ii+1]['dout'], A[ii]['cache'])
+                gvs =         l.dfa_gv(A[ii-1]['aout'], A[ii]['aout'], E, D[ii+1]['dout'], D[ii]['cache'])
                 grads_and_vars.extend(gvs)
                 
         return grads_and_vars
@@ -204,27 +204,27 @@ class Model:
             if ii == 0:
                 A[ii] = l.forward(X)
             else:
-                A[ii] = l.forward(A[ii-1])
-            
-        E = tf.nn.softmax(A[self.num_layers-1]) - Y
-        N = tf.shape(A[self.num_layers-1])[0]
+                A[ii] = l.forward(A[ii-1]['aout'])
+
+        E = tf.nn.softmax(A[self.num_layers-1]['aout']) - Y
+        N = tf.shape(A[self.num_layers-1]['aout'])[0]
         N = tf.cast(N, dtype=tf.float32)
         E = E / N
             
         for ii in range(self.num_layers-1, -1, -1):
             l = self.layers[ii]
-                
+
             if (ii == self.num_layers-1):
-                D[ii] = l.lel_backward(A[ii-1], A[ii], E, E, Y)
-                gvs = l.lel_gv(A[ii-1], A[ii], E, E, Y)
+                D[ii] = l.lel_backward(A[ii-1]['aout'], A[ii]['aout'], E, E,               A[ii]['cache'], Y)
+                gvs =         l.dfa_gv(A[ii-1]['aout'], A[ii]['aout'], E, E,               D[ii]['cache'], Y)
                 grads_and_vars.extend(gvs)
             elif (ii == 0):
-                D[ii] = l.lel_backward(X, A[ii], E, D[ii+1], Y)
-                gvs = l.lel_gv(X, A[ii], E, D[ii+1], Y)
+                D[ii] = l.lel_backward(X,               A[ii]['aout'], E, D[ii+1]['dout'], A[ii]['cache'], Y)
+                gvs =         l.dfa_gv(X,               A[ii]['aout'], E, D[ii+1]['dout'], D[ii]['cache'], Y)
                 grads_and_vars.extend(gvs)
             else:
-                D[ii] = l.lel_backward(A[ii-1], A[ii], E, D[ii+1], Y)
-                gvs = l.lel_gv(A[ii-1], A[ii], E, D[ii+1], Y)
+                D[ii] = l.lel_backward(A[ii-1]['aout'], A[ii]['aout'], E, D[ii+1]['dout'], A[ii]['cache'], Y)
+                gvs =         l.dfa_gv(A[ii-1]['aout'], A[ii]['aout'], E, D[ii+1]['dout'], D[ii]['cache'], Y)
                 grads_and_vars.extend(gvs)
                 
         return grads_and_vars
@@ -263,30 +263,31 @@ class Model:
     def dfa_backwards(self, X, Y):
         A = [None] * self.num_layers
         D = [None] * self.num_layers
+        grads_and_vars = []
         
         for ii in range(self.num_layers):
             l = self.layers[ii]
             if ii == 0:
                 A[ii] = l.forward(X)
             else:
-                A[ii] = l.forward(A[ii-1])
+                A[ii] = l.forward(A[ii-1]['aout'])
             
-        E = tf.nn.softmax(A[self.num_layers-1]) - Y
-        N = tf.shape(A[self.num_layers-1])[0]
+        E = tf.nn.softmax(A[self.num_layers-1]['aout']) - Y
+        N = tf.shape(A[self.num_layers-1]['aout'])[0]
         N = tf.cast(N, dtype=tf.float32)
         E = E / N
             
         for ii in range(self.num_layers-1, -1, -1):
             l = self.layers[ii]
-                
+            
             if (ii == self.num_layers-1):
-                D[ii] = l.dfa_backward(A[ii-1], A[ii], E, E)
+                D[ii] = l.dfa_backward(A[ii-1]['aout'], A[ii]['aout'], E, A[ii]['cache'])
             elif (ii == 0):
-                D[ii] = l.dfa_backward(X, A[ii], E, D[ii+1])
+                D[ii] = l.dfa_backward(X, A[ii]['aout'], D[ii+1]['dout'], A[ii]['cache'])
             else:
-                D[ii] = l.dfa_backward(A[ii-1], A[ii], E, D[ii+1])
+                D[ii] = l.dfa_backward(A[ii-1]['aout'], A[ii]['aout'], D[ii+1]['dout'], A[ii]['cache'])
                 
-        return D
+        return D[0]['dout']
     
     ####################################################################
     
