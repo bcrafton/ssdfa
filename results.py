dd0b2f5b4259db189a31afdad5dbd2fc8c2c891b
Brian Crafton <crafton.b@husky.neu.edu>
2019-06-22 16:12:01 -0400

    changing VGG choice

--- results.py
+++ results.py
@@ -99,7 +99,7 @@ vgg64_lel_2fc = {'benchmark':'vgg64_lel_2fc.py', 'epochs':30, 'batch_size':64, '
 
 ################################################
 
-vgg64_ae = {'benchmark':'vgg64_autoencoder19.py', 'epochs':10, 'batch_size':32, 'alpha':[0.01], 'l2':[0.], 'eps':[1.], 'act':['relu'], 'bias':[0.], 'dropout':[0.5], 'dfa':0, 'sparse':0, 'rank':0, 'init':['alexnet'], 'opt':'adam', 'load':[None, 'vgg64_bp.npy', 'vgg64_lel.npy']}
+vgg64_ae = {'benchmark':'vgg64_autoencoder22.py', 'epochs':10, 'batch_size':32, 'alpha':[0.01], 'l2':[0.], 'eps':[1.], 'act':['relu'], 'bias':[0.], 'dropout':[0.5], 'dfa':0, 'sparse':0, 'rank':0, 'init':['alexnet'], 'opt':'adam', 'load':['vgg64_bp.npy', 'vgg64_lel.npy']}
 
 vgg64_ae_scratch = {'benchmark':'vgg64_autoencoder18.py', 'epochs':10, 'batch_size':32, 'alpha':[0.001, 0.0001], 'l2':[0.], 'eps':[1.], 'act':['relu'], 'bias':[0.], 'dropout':[0.5], 'dfa':0, 'sparse':0, 'rank':0, 'init':['alexnet'], 'opt':'adam', 'load':None}
 
@@ -109,18 +109,16 @@ vgg64_pool = {'benchmark':'vgg64_pool.py', 'epochs':10, 'batch_size':64, 'alpha'
 
 mobile_net_224 = {'benchmark':'MobileNet224.py', 'epochs':100, 'batch_size':64, 'alpha':[1e-1], 'l2':[0.], 'eps':[1.], 'act':['relu'], 'bias':[0.], 'dropout':[0.5], 'dfa':0, 'sparse':0, 'rank':0, 'init':['alexnet'], 'opt':'adam', 'load':None}
 
-vgg_net_224 = {'benchmark':'VGG_small_224.py', 'epochs':100, 'batch_size':32, 'alpha':[1e-1], 'l2':[0.], 'eps':[1.], 'act':['relu'], 'bias':[0.], 'dropout':[0.5], 'dfa':0, 'sparse':0, 'rank':0, 'init':['alexnet'], 'opt':'adam', 'load':None}
+vgg_net_224 = {'benchmark':'VGG224_2.py', 'epochs':100, 'batch_size':64, 'alpha':[1e-1], 'l2':[0.], 'eps':[1.], 'act':['relu'], 'bias':[0.], 'dropout':[0.5], 'dfa':0, 'sparse':0, 'rank':0, 'init':['alexnet'], 'opt':'adam', 'load':None}
 
 ################################################
 
 # params = [vgg64_ae, vgg64_pool]
 # params = [vgg64_pool]
-# params = [vgg64_ae]
+params = [vgg64_ae]
 # params = [vgg64_ae_scratch]
-# params = [mobile_net_224]
-# params = [vgg64_ae_scratch, vgg64_pool]
 
-params = [mobile_net_224, vgg_net_224]
+# params = [mobile_net_224, vgg_net_224]
 
 ################################################
 
