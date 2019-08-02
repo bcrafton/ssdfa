ed39e218d76977308d50f188185f6cbdc4bf4002
Brian Crafton <crafton.b@husky.neu.edu>
2019-08-02 10:53:10 -0400

    cifar10 bp runs

--- cifar10_conv.py
+++ cifar10_conv.py
@@ -84,26 +84,31 @@ Y = tf.placeholder(tf.float32, [None, 10])
 
 l0 = Convolution(input_shape=[batch_size, 32, 32, 3], filter_sizes=[5, 5, 3, 96], init=args.init, activation=act, bias=args.bias, name='conv1')
 l1 = MaxPool(size=[batch_size, 32, 32, 96], ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="SAME")
+l2 = FeedbackConv(size=[batch_size, 16, 16, 96], num_classes=10, sparse=args.sparse, rank=args.rank, name='conv1_fb')
 
-l2 = Convolution(input_shape=[batch_size, 16, 16, 96], filter_sizes=[5, 5, 96, 128], init=args.init, activation=act, bias=args.bias, name='conv2')
-l3 = MaxPool(size=[batch_size, 16, 16, 128], ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="SAME")
+l3 = Convolution(input_shape=[batch_size, 16, 16, 96], filter_sizes=[5, 5, 96, 128], init=args.init, activation=act, bias=args.bias, name='conv2')
+l4 = MaxPool(size=[batch_size, 16, 16, 128], ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="SAME")
+l5 = FeedbackConv(size=[batch_size, 8, 8, 128], num_classes=10, sparse=args.sparse, rank=args.rank, name='conv2_fb')
 
-l4 = Convolution(input_shape=[batch_size, 8, 8, 128], filter_sizes=[5, 5, 128, 256], init=args.init, activation=act, bias=args.bias, name='conv3')
-l5 = MaxPool(size=[batch_size, 8, 8, 256], ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="SAME")
+l6 = Convolution(input_shape=[batch_size, 8, 8, 128], filter_sizes=[5, 5, 128, 256], init=args.init, activation=act, bias=args.bias, name='conv3')
+l7 = MaxPool(size=[batch_size, 8, 8, 256], ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="SAME")
+l8 = FeedbackConv(size=[batch_size, 4, 4, 256], num_classes=10, sparse=args.sparse, rank=args.rank, name='conv3_fb')
 
-l6 = ConvToFullyConnected(input_shape=[4, 4, 256])
+l9 = ConvToFullyConnected(input_shape=[4, 4, 256])
 
-l7 = FullyConnected(input_shape=4*4*256, size=2048, init=args.init, activation=act, bias=args.bias, name='fc1')
-l8 = Dropout(rate=dropout_rate)
+l10 = FullyConnected(input_shape=4*4*256, size=2048, init=args.init, activation=act, bias=args.bias, name='fc1')
+l11 = Dropout(rate=dropout_rate)
+l12 = FeedbackFC(size=[4*4*256, 2048], num_classes=10, sparse=args.sparse, rank=args.rank, name='fc1_fb')
 
-l9 = FullyConnected(input_shape=2048, size=2048, init=args.init, activation=act, bias=args.bias, name='fc2')
-l10 = Dropout(rate=dropout_rate)
+l13 = FullyConnected(input_shape=2048, size=2048, init=args.init, activation=act, bias=args.bias, name='fc2')
+l14 = Dropout(rate=dropout_rate)
+l15 = FeedbackFC(size=[2048, 2048], num_classes=10, sparse=args.sparse, rank=args.rank, name='fc2_fb')
 
-l11 = FullyConnected(input_shape=2048, size=10, init=args.init, bias=args.bias, name='fc3')
+l16 = FullyConnected(input_shape=2048, size=10, init=args.init, bias=args.bias, name='fc3')
 
 ##############################################
 
-model = Model(layers=[l0, l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11])
+model = Model(layers=[l0, l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13, l14, l15, l16])
 predict = model.predict(X=X)
 weights = model.get_weights()
 
@@ -137,8 +142,6 @@ train_accs = []
 test_accs = []
 
 for ii in range(args.epochs):
-
-    print (ii)
 
     #############################
     
@@ -176,7 +179,7 @@ for ii in range(args.epochs):
     
     #############################
             
-    p = "train acc: %f test acc: %f" % (train_acc, test_acc)
+    p = "%d | train acc: %f | test acc: %f" % (ii, train_acc, test_acc)
     print (p)
     f = open(filename, "a")
     f.write(p + "\n")
