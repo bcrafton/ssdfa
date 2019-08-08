0c2103efe7e061aa335e44e2c7ffb775d3fb7797
Brian Crafton <crafton.b@husky.neu.edu>
2019-08-08 18:12:35 -0400

    vgg block

--- lib/VGGBlock.py
+++ lib/VGGBlock.py
@@ -4,12 +4,10 @@ import numpy as np
 
 from lib.Layer import Layer 
 from lib.ConvBlock import ConvBlock
-# from lib.LELConv import LELConv
-from lib.LELPool import LELPool
 
 class VGGBlock(Layer):
 
-    def __init__(self, input_shape, filter_shape, strides, init, pool_shape, num_classes, name, load=None, train=True, ae_loss=0):
+    def __init__(self, input_shape, filter_shape, strides, init, name, load=None, train=True):
         self.input_shape = input_shape
         self.batch, self.h, self.w, self.fin = self.input_shape
         
@@ -20,16 +18,9 @@ class VGGBlock(Layer):
         _, self.sh, self.sw, _ = self.strides
 
         self.init = init
-
-        self.pool_shape = pool_shape
-        self.num_classes = num_classes
-        
         self.name = name
         self.load = load
         self.train_flag = train
-        self.ae_loss = ae_loss
-
-        self.lel_shape = [self.batch, self.h // self.sh, self.w // self.sw, self.fout]
         
         self.conv = ConvBlock(input_shape=self.input_shape, 
                               filter_shape=[3, 3, self.fin, self.fout], 
@@ -39,20 +30,11 @@ class VGGBlock(Layer):
                               load=self.load, 
                               train=self.train_flag)
 
-        self.lel = LELPool(input_shape=self.lel_shape, 
-                           pool_shape=self.pool_shape, 
-                           num_classes=self.num_classes, 
-                           ae_output_shape=self.input_shape, 
-                           ae_filter_shape=[3, 3, self.fout, self.fin],
-                           name=self.name + '_lel',
-                           ae_loss=self.ae_loss)
-
     ###################################################################
 
     def get_weights(self):
         weights = []
         weights.extend(self.conv.get_weights())
-        # should add lel weights here, but we dont need them atm.
         return weights
 
     def output_shape(self):
@@ -61,8 +43,6 @@ class VGGBlock(Layer):
     def num_params(self):
         return self.conv.num_params()
 
-    ###################################################################
-
     def forward(self, X):
         conv = self.conv.forward(X)
         lel = self.lel.forward(conv['aout'])
@@ -70,85 +50,21 @@ class VGGBlock(Layer):
         cache = {'conv':conv, 'lel':lel}
         return {'aout':conv['aout'], 'cache':cache}
         
-    def backward(self, AI, AO, DO, cache):    
-        
-        conv = cache['conv']
-        
-        ##########################################3
-        
-        dconv = self.conv.backward(AI, conv['aout'], DO, conv['cache'])
-
-        ##########################################
-
-        cache.update({'dconv':dconv})
-        return {'dout':dconv['dout'], 'cache':cache}
-        
-    def gv(self, AI, AO, DO, cache):
-
-        conv = cache['conv']
-        dconv = cache['dconv']
-        
-        ##########################################
-
-        dconv = self.conv.gv(AI, conv['aout'], DO, dconv['cache'])
-        
-        ##########################################
-        
-        grads = []
-        grads.extend(dconv)
-        return grads
-        
-    def train(self, AI, AO, DO): 
-        assert(False)
-        
     ###################################################################
-
-    def dfa_backward(self, AI, AO, E, DO):
-        assert(False)
         
-    def dfa_gv(self, AI, AO, E, DO):
-        assert(False)
-        
-    def dfa(self, AI, AO, E, DO): 
-        assert(False)
-        
-    ###################################################################   
-    
-    def lel_backward(self, AI, AO, E, DO, Y, cache):
-    
-        conv, lel = cache['conv'], cache['lel']
-        
-        ##########################################
-        
-        lel['cache'].update({'AE_X': AI})
-        
-        dlel = self.lel.lel_backward(conv['aout'], lel['aout'], None, DO, Y, lel['cache'])
-        dconv = self.conv.lel_backward(AI, conv['aout'], None, dlel['dout'], Y, conv['cache'])
-
-        ##########################################
-
-        cache.update({'dconv':dconv, 'dlel':dlel})
-        return {'dout':dconv['dout'], 'cache':cache}
-
-    def lel_gv(self, AI, AO, E, DO, Y, cache):
-
-        conv, lel  = cache['conv'], cache['lel']
-        dconv, dlel = cache['dconv'], cache['dlel']
-        
-        ##########################################
-
-        dconv = self.conv.lel_gv(AI, conv['aout'], None, dlel['dout'], Y, dconv['cache'])
-        dlel= self.lel.lel_gv(conv['aout'], lel['aout'], None, DO, Y, dlel['cache'])
-        
-        ##########################################
-        
+    def bp(self, AI, AO, DO, cache):    
+        conv = cache['conv']
+        dconv, gconv = self.conv.backward(AI, conv['aout'], DO, conv['cache'])
+        cache.update({'dconv':dconv})
         grads = []
-        grads.extend(dconv)
-        grads.extend(dlel)
-        return grads
+        grads.extend(gconv)
+        return {'dout':dconv['dout'], 'cache':cache}, grads
         
-    def lel(self, AI, AO, E, DO, Y): 
-        assert(False)
+    def dfa(self, AI, AO, DO, cache):    
+        return self.bp(AI, AO, DO, cache)
+    
+    def lel(self, AI, AO, DO, cache):
+        return self.bp(AI, AO, DO, cache)
         
     ###################################################################   
     
