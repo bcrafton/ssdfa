faec33c81bee73437340bc2f827da2e5b208aca1
Brian Crafton <crafton.b@husky.neu.edu>
2019-08-09 17:49:03 -0400

    cifar10

---
+++ cifar10.py
@@ -0,0 +1,172 @@
+
+import argparse
+import os
+import sys
+
+##############################################
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--epochs', type=int, default=100)
+parser.add_argument('--batch_size', type=int, default=64)
+parser.add_argument('--lr', type=float, default=1e-4)
+parser.add_argument('--eps', type=float, default=1e-5)
+parser.add_argument('--dropout', type=float, default=0.5)
+parser.add_argument('--bias', type=float, default=0.)
+parser.add_argument('--gpu', type=int, default=0)
+parser.add_argument('--dfa', type=int, default=0)
+parser.add_argument('--sparse', type=int, default=0)
+parser.add_argument('--rank', type=int, default=0)
+parser.add_argument('--init', type=str, default="glorot_uniform")
+parser.add_argument('--save', type=int, default=0)
+parser.add_argument('--name', type=str, default="cifar10_conv")
+parser.add_argument('--load', type=str, default=None)
+args = parser.parse_args()
+
+if args.gpu >= 0:
+    os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"]=str(args.gpu)
+
+##############################################
+
+import tensorflow as tf
+import keras
+import numpy as np
+
+from lib.Model import Model
+
+from lib.Layer import Layer 
+from lib.ConvToFullyConnected import ConvToFullyConnected
+from lib.FullyConnected import FullyConnected
+from lib.Convolution import Convolution
+from lib.MaxPool import MaxPool
+from lib.Dropout import Dropout
+from lib.FeedbackFC import FeedbackFC
+from lib.FeedbackConv import FeedbackConv
+
+from lib.Activation import Relu
+from lib.Activation import Tanh
+
+from lib.cifar import cifar_conv
+from lib.cifar import cifar_fc
+
+##############################################
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+train_examples = 50000
+test_examples = 10000
+
+assert(np.shape(x_train) == (train_examples, 32, 32, 3))
+x_train = x_train - np.mean(x_train, axis=0, keepdims=True)
+x_train = x_train / np.std(x_train, axis=0, keepdims=True)
+y_train = keras.utils.to_categorical(y_train, 10)
+
+assert(np.shape(x_test) == (test_examples, 32, 32, 3))
+x_test = x_test - np.mean(x_test, axis=0, keepdims=True)
+x_test = x_test / np.std(x_test, axis=0, keepdims=True)
+y_test = keras.utils.to_categorical(y_test, 10)
+
+##############################################
+
+tf.set_random_seed(0)
+tf.reset_default_graph()
+
+batch_size = tf.placeholder(tf.int32, shape=())
+dropout_rate = tf.placeholder(tf.float32, shape=())
+lr = tf.placeholder(tf.float32, shape=())
+
+X = tf.placeholder(tf.float32, [None, 32, 32, 3])
+Y = tf.placeholder(tf.float32, [None, 10])
+
+model = cifar_conv(batch_size=batch_size, dropout_rate=dropout_rate)
+# model = cifar_fc(batch_size=batch_size, dropout_rate=dropout_rate)
+
+##############################################
+
+predict = model.predict(X=X)
+weights = model.get_weights()
+
+if args.dfa:
+    grads_and_vars = model.dfa_gvs(X=X, Y=Y)
+else:
+    grads_and_vars = model.gvs(X=X, Y=Y)
+        
+train = tf.train.AdamOptimizer(learning_rate=lr, epsilon=args.eps).apply_gradients(grads_and_vars=grads_and_vars)
+
+correct = tf.equal(tf.argmax(predict,1), tf.argmax(Y,1))
+total_correct = tf.reduce_sum(tf.cast(correct, tf.float32))
+
+##############################################
+
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+tf.local_variables_initializer().run()
+
+##############################################
+
+filename = args.name + '.results'
+f = open(filename, "w")
+f.write(filename + "\n")
+f.write("total params: " + str(model.num_params()) + "\n")
+f.close()
+
+##############################################
+
+train_accs = []
+test_accs = []
+
+for ii in range(args.epochs):
+
+    #############################
+    
+    _total_correct = 0
+    for jj in range(0, train_examples, args.batch_size):
+        s = jj
+        e = min(jj + args.batch_size, train_examples)
+        b = e - s
+        
+        xs = x_train[s:e]
+        ys = y_train[s:e]
+        
+        _correct, _ = sess.run([total_correct, train], feed_dict={batch_size: b, dropout_rate: args.dropout, lr: args.lr, X: xs, Y: ys})
+        _total_correct += _correct
+
+    train_acc = 1.0 * _total_correct / (train_examples - (train_examples % args.batch_size))
+    train_accs.append(train_acc)
+
+    #############################
+
+    _total_correct = 0
+    for jj in range(0, test_examples, args.batch_size):
+        s = jj
+        e = min(jj + args.batch_size, test_examples)
+        b = e - s
+        
+        xs = x_test[s:e]
+        ys = y_test[s:e]
+        
+        _correct = sess.run(total_correct, feed_dict={batch_size: b, dropout_rate: 0.0, lr: 0.0, X: xs, Y: ys})
+        _total_correct += _correct
+        
+    test_acc = 1.0 * _total_correct / (test_examples - (test_examples % args.batch_size))
+    test_accs.append(test_acc)
+    
+    #############################
+            
+    p = "%d | train acc: %f | test acc: %f" % (ii, train_acc, test_acc)
+    print (p)
+    f = open(filename, "a")
+    f.write(p + "\n")
+    f.close()
+
+##############################################
+
+if args.save:
+    [w] = sess.run([weights], feed_dict={})
+    w['train_acc'] = train_accs
+    w['test_acc'] = test_accs
+    np.save(args.name, w)
+    
+##############################################
+
+
---
+++ lib/cifar.py
@@ -0,0 +1,74 @@
+
+import tensorflow as tf
+import keras
+import numpy as np
+
+from lib.Model import Model
+
+from lib.Layer import Layer 
+from lib.ConvToFullyConnected import ConvToFullyConnected
+from lib.FullyConnected import FullyConnected
+from lib.Convolution import Convolution
+from lib.MaxPool import MaxPool
+from lib.Dropout import Dropout
+from lib.FeedbackFC import FeedbackFC
+from lib.FeedbackConv import FeedbackConv
+
+from lib.Activation import Relu
+from lib.Activation import Tanh
+
+def cifar_conv(batch_size, dropout_rate, init='alexnet', sparse=0, bias=0.1, num_classes=10):
+    l0  = Convolution(input_shape=[batch_size, 32, 32, 3], filter_sizes=[5, 5, 3, 96], init=init, bias=bias, name='conv1')
+    l1  = Relu()
+    l2  = MaxPool(size=[batch_size, 32, 32, 96], ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="SAME")
+    l3  = FeedbackConv(size=[batch_size, 16, 16, 96], num_classes=num_classes, sparse=sparse, name='conv1_fb')
+
+    l4  = Convolution(input_shape=[batch_size, 16, 16, 96], filter_sizes=[5, 5, 96, 128], init=init, bias=bias, name='conv2')
+    l5  = Relu()
+    l6  = MaxPool(size=[batch_size, 16, 16, 128], ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="SAME")
+    l7  = FeedbackConv(size=[batch_size, 8, 8, 128], num_classes=num_classes, sparse=sparse, name='conv2_fb')
+
+    l8  = Convolution(input_shape=[batch_size, 8, 8, 128], filter_sizes=[5, 5, 128, 256], init=init, bias=bias, name='conv3')
+    l9  = Relu()
+    l10 = MaxPool(size=[batch_size, 8, 8, 256], ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="SAME")
+    l11 = FeedbackConv(size=[batch_size, 4, 4, 256], num_classes=num_classes, sparse=sparse, name='conv3_fb')
+
+    l12 = ConvToFullyConnected(input_shape=[4, 4, 256])
+
+    l13 = FullyConnected(input_shape=4*4*256, size=2048, init=init, bias=bias, name='fc1')
+    l14 = Relu()
+    l15 = Dropout(rate=dropout_rate)
+    l16 = FeedbackFC(size=[4*4*256, 2048], num_classes=num_classes, sparse=sparse, name='fc1_fb')
+
+    l17 = FullyConnected(input_shape=2048, size=2048, init=init, bias=bias, name='fc2')
+    l18 = Relu()
+    l19 = Dropout(rate=dropout_rate)
+    l20 = FeedbackFC(size=[2048, 2048], num_classes=num_classes, sparse=sparse, name='fc2_fb')
+
+    l21 = FullyConnected(input_shape=2048, size=num_classes, init=init, bias=bias, name='fc3')
+
+    layers=[l0, l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13, l14, l15, l16, l17, l18, l19, l20, l21]
+    model = Model(layers=layers)
+    return model
+
+def cifar_fc(batch_size, dropout_rate, init='alexnet', sparse=0, bias=0.1, num_classes=10):
+    l0 = ConvToFullyConnected(input_shape=[32, 32, 3])
+    l1 = Dropout(rate=0.1)
+
+    l2 = FullyConnected(input_shape=3072, size=1000, init=init, activation=act, bias=bias, name='fc1')
+    l3 = Dropout(rate=dropout_rate)
+    l4 = FeedbackFC(size=[3072, 1000], num_classes=num_classes, sparse=sparse, name='fc1_fb')
+
+    l5 = FullyConnected(input_shape=1000, size=1000, init=init, activation=act, bias=bias, name='fc2')
+    l6 = Dropout(rate=dropout_rate)
+    l7 = FeedbackFC(size=[1000, 1000], num_classes=num_classes, sparse=sparse, name='fc2_fb')
+
+    l8 = FullyConnected(input_shape=1000, size=1000, init=init, activation=act, bias=bias, name='fc3')
+    l9 = Dropout(rate=dropout_rate)
+    l10 = FeedbackFC(size=[1000, 1000], num_classes=num_classes, sparse=sparse, name='fc3_fb')
+
+    l11 = FullyConnected(input_shape=1000, size=num_classes, init=init, bias=bias, name='fc4')
+
+    model = Model(layers=[l0, l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11])
+    return model
+
