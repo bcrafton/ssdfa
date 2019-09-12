4a22b1c1e612a8ede3343d96dc2b69ab8763a12b
Brian Crafton <crafton.b@husky.neu.edu>
2019-09-12 16:55:31 -0400

    this uses too much memory. try {multi-gpus, exp vs act memory}

--- lib/DenseNet.py
+++ lib/DenseNet.py
@@ -14,12 +14,15 @@ from lib.ConvToFullyConnected import ConvToFullyConnected
 
 def DenseNet64(batch_size, dropout_rate, init='alexnet'):
 
+    # 856 = 6*12 + 12*12 + 24*12 + 24*12 + 64
+    # 1920 = 6*32 + 12*32 + 24*32 + 16*32 + 64 --- too big apparently.
+
     l0 = BatchNorm(input_size=[batch_size, 64, 64, 3], name='bn0')
     l1 = ConvBlock(input_shape=[batch_size, 64, 64, 3], filter_shape=[3, 3, 3, 64], strides=[1,1,1,1], init=init, name='conv1')
-    l2 = DenseModel(input_shape=[batch_size, 64, 64, 64], init=init, name='dense_model', k=12, L=[6, 12, 24, 24])
-    l3 = AvgPool(size=[batch_size, 4, 4, 856], ksize=[1,4,4,1], strides=[1,4,4,1], padding='SAME')
-    l4 = ConvToFullyConnected(input_shape=[1, 1, 856])
-    l5 = FullyConnected(input_shape=856, size=1000, init=init, name="fc1")
+    l2 = DenseModel(input_shape=[batch_size, 64, 64, 64], init=init, name='dense_model', k=32, L=[6, 12, 24, 16])
+    l3 = AvgPool(size=[batch_size, 4, 4, 1920], ksize=[1,4,4,1], strides=[1,4,4,1], padding='SAME')
+    l4 = ConvToFullyConnected(input_shape=[1, 1, 1920]) 
+    l5 = FullyConnected(input_shape=1920, size=1000, init=init, name="fc1")
     layers = [l0, l1, l2, l3, l4, l5]
     model = Model(layers=layers)
     return model
