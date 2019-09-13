4ef8d71cb3ead9aab763daa49787d3e6169e768e
Brian A Crafton <bcrafton3@KL2125-01.ece.gatech.edu>
2019-09-13 09:56:25 -0400

    dont need mutliple gpus ... why did it not run faster

--- lib/DenseModelMultiGPU.py
+++ lib/DenseModelMultiGPU.py
@@ -43,19 +43,19 @@ class DenseModel(Layer):
             trans_fmaps = self.fin + L[0] * k
             trans1 = DenseTransition(input_shape=[self.batch, self.h, self.w, trans_fmaps], init=self.init, name=self.name + '_trans_1')
 
-        with tf.device('/device:GPU:1'):
+        with tf.device('/device:GPU:0'):
             dense_fmaps = self.fin + L[0]          * k
             dense2 = DenseBlock(input_shape=[self.batch, self.h // 2, self.w // 2, dense_fmaps], init=self.init, name=self.name + '_block_2', k=self.k, L=self.L[1])
             trans_fmaps = self.fin + (L[0]+L[1]) * k
             trans2 = DenseTransition(input_shape=[self.batch, self.h // 2, self.w // 2, trans_fmaps], init=self.init, name=self.name + '_trans_2')
 
-        with tf.device('/device:GPU:2'):
+        with tf.device('/device:GPU:0'):
             dense_fmaps = self.fin + (L[0]+L[1])      * k
             dense3 = DenseBlock(input_shape=[self.batch, self.h // 4, self.w // 4, dense_fmaps], init=self.init, name=self.name + '_block_3', k=self.k, L=self.L[2])
             trans_fmaps = self.fin + (L[0]+L[1]+L[2]) * k
             trans3 = DenseTransition(input_shape=[self.batch, self.h // 4, self.w // 4, trans_fmaps], init=self.init, name=self.name + '_trans_3')
 
-        with tf.device('/device:GPU:3'):
+        with tf.device('/device:GPU:0'):
             dense_fmaps = self.fin + + (L[0]+L[1]+L[2]) * k
             dense4 = DenseBlock(input_shape=[self.batch, self.h // 8, self.w // 8, dense_fmaps], init=self.init, name=self.name + '_block_4', k=self.k, L=self.L[3])
         
@@ -87,15 +87,15 @@ class DenseModel(Layer):
             A[0], cache[0] = self.blocks[0].forward(X)
             A[1], cache[1] = self.blocks[1].forward(A[0])
 
-        with tf.device('/device:GPU:1'):
+        with tf.device('/device:GPU:0'):
             A[2], cache[2] = self.blocks[2].forward(A[1])
             A[3], cache[3] = self.blocks[3].forward(A[2])
 
-        with tf.device('/device:GPU:2'):
+        with tf.device('/device:GPU:0'):
             A[4], cache[4] = self.blocks[4].forward(A[3])
             A[5], cache[5] = self.blocks[5].forward(A[4])
 
-        with tf.device('/device:GPU:3'):
+        with tf.device('/device:GPU:0'):
             A[6], cache[6] = self.blocks[6].forward(A[5])
 
         return A[self.num_blocks-1], (A, cache)
@@ -111,14 +111,14 @@ class DenseModel(Layer):
         D = [None] * self.num_blocks
         GV = []
 
-        with tf.device('/device:GPU:3'):
+        with tf.device('/device:GPU:0'):
             D[6], gv6 = self.blocks[6].bp(A[5], A[6], DO,      C[6])
 
-        with tf.device('/device:GPU:2'):
+        with tf.device('/device:GPU:0'):
             D[5], gv5 = self.blocks[5].bp(A[4], A[5], D[6],    C[5])
             D[4], gv4 = self.blocks[4].bp(A[3], A[4], D[5],    C[4])
 
-        with tf.device('/device:GPU:1'):
+        with tf.device('/device:GPU:0'):
             D[3], gv3 = self.blocks[3].bp(A[2], A[3], D[4],    C[3])
             D[2], gv2 = self.blocks[2].bp(A[1], A[2], D[3],    C[2])
 
