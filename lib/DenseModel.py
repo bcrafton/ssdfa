e0d7af2fa9106845fa9e06ed7076835ee80f58f2
Brian Crafton <crafton.b@husky.neu.edu>
2019-09-16 13:43:41 -0400

    hope this works

--- lib/DenseModel.py
+++ lib/DenseModel.py
@@ -34,41 +34,72 @@ class DenseModel(Layer):
 
         #####################################
 
-        self.blocks = []
+        if len(L) == 4:
+            self.blocks = []
 
-        dense_fmaps = self.fin 
-        dense1 = DenseBlock(input_shape=[self.batch, self.h, self.w, dense_fmaps], init=self.init, name=self.name + '_block_1', k=self.k, L=self.L[0])
-        trans_fmaps = self.fin + L[0] * k
-        trans1 = DenseTransition(input_shape=[self.batch, self.h, self.w, trans_fmaps], init=self.init, name=self.name + '_trans_1')
+            dense_fmaps = self.fin 
+            dense1 = DenseBlock(input_shape=[self.batch, self.h, self.w, dense_fmaps], init=self.init, name=self.name + '_block_1', k=self.k, L=self.L[0])
+            trans_fmaps = self.fin + L[0] * k
+            trans1 = DenseTransition(input_shape=[self.batch, self.h, self.w, trans_fmaps], init=self.init, name=self.name + '_trans_1')
 
-        dense_fmaps = self.fin + L[0] * k
-        dense2 = DenseBlock(input_shape=[self.batch, self.h // 2, self.w // 2, dense_fmaps], init=self.init, name=self.name + '_block_2', k=self.k, L=self.L[1])
-        trans_fmaps = self.fin + (L[0]+L[1]) * k
-        trans2 = DenseTransition(input_shape=[self.batch, self.h // 2, self.w // 2, trans_fmaps], init=self.init, name=self.name + '_trans_2')
+            dense_fmaps = self.fin + L[0] * k
+            dense2 = DenseBlock(input_shape=[self.batch, self.h // 2, self.w // 2, dense_fmaps], init=self.init, name=self.name + '_block_2', k=self.k, L=self.L[1])
+            trans_fmaps = self.fin + (L[0]+L[1]) * k
+            trans2 = DenseTransition(input_shape=[self.batch, self.h // 2, self.w // 2, trans_fmaps], init=self.init, name=self.name + '_trans_2')
 
-        dense_fmaps = self.fin + (L[0]+L[1]) * k
-        dense3 = DenseBlock(input_shape=[self.batch, self.h // 4, self.w // 4, dense_fmaps], init=self.init, name=self.name + '_block_3', k=self.k, L=self.L[2])
-        trans_fmaps = self.fin + (L[0]+L[1]+L[2]) * k
-        trans3 = DenseTransition(input_shape=[self.batch, self.h // 4, self.w // 4, trans_fmaps], init=self.init, name=self.name + '_trans_3')
+            dense_fmaps = self.fin + (L[0]+L[1]) * k
+            dense3 = DenseBlock(input_shape=[self.batch, self.h // 4, self.w // 4, dense_fmaps], init=self.init, name=self.name + '_block_3', k=self.k, L=self.L[2])
+            trans_fmaps = self.fin + (L[0]+L[1]+L[2]) * k
+            trans3 = DenseTransition(input_shape=[self.batch, self.h // 4, self.w // 4, trans_fmaps], init=self.init, name=self.name + '_trans_3')
 
-        dense_fmaps = self.fin + (L[0]+L[1]+L[2]) * k
-        dense4 = DenseBlock(input_shape=[self.batch, self.h // 8, self.w // 8, dense_fmaps], init=self.init, name=self.name + '_block_4', k=self.k, L=self.L[3])
-        trans_fmaps = self.fin + (L[0]+L[1]+L[2]+L[3]) * k
-        trans4 = DenseTransition(input_shape=[self.batch, self.h // 8, self.w // 8, trans_fmaps], init=self.init, name=self.name + '_trans_4')
+            dense_fmaps = self.fin + (L[0]+L[1]+L[2]) * k
+            dense4 = DenseBlock(input_shape=[self.batch, self.h // 8, self.w // 8, dense_fmaps], init=self.init, name=self.name + '_block_4', k=self.k, L=self.L[3])
+            
+            self.blocks.append(dense1)
+            self.blocks.append(trans1)
+            self.blocks.append(dense2)
+            self.blocks.append(trans2)
+            self.blocks.append(dense3)
+            self.blocks.append(trans3)
+            self.blocks.append(dense4)
+            self.num_blocks = len(self.blocks)
 
-        dense_fmaps = self.fin + + (L[0]+L[1]+L[2]+L[3]) * k
-        dense5 = DenseBlock(input_shape=[self.batch, self.h // 16, self.w // 16, dense_fmaps], init=self.init, name=self.name + '_block_5', k=self.k, L=self.L[4])
-        
-        self.blocks.append(dense1)
-        self.blocks.append(trans1)
-        self.blocks.append(dense2)
-        self.blocks.append(trans2)
-        self.blocks.append(dense3)
-        self.blocks.append(trans3)
-        self.blocks.append(dense4)
-        self.blocks.append(trans4)
-        self.blocks.append(dense5)
-        self.num_blocks = len(self.blocks)
+        if len(L) == 5:
+            self.blocks = []
+
+            dense_fmaps = self.fin 
+            dense1 = DenseBlock(input_shape=[self.batch, self.h, self.w, dense_fmaps], init=self.init, name=self.name + '_block_1', k=self.k, L=self.L[0])
+            trans_fmaps = self.fin + L[0] * k
+            trans1 = DenseTransition(input_shape=[self.batch, self.h, self.w, trans_fmaps], init=self.init, name=self.name + '_trans_1')
+
+            dense_fmaps = self.fin + L[0] * k
+            dense2 = DenseBlock(input_shape=[self.batch, self.h // 2, self.w // 2, dense_fmaps], init=self.init, name=self.name + '_block_2', k=self.k, L=self.L[1])
+            trans_fmaps = self.fin + (L[0]+L[1]) * k
+            trans2 = DenseTransition(input_shape=[self.batch, self.h // 2, self.w // 2, trans_fmaps], init=self.init, name=self.name + '_trans_2')
+
+            dense_fmaps = self.fin + (L[0]+L[1]) * k
+            dense3 = DenseBlock(input_shape=[self.batch, self.h // 4, self.w // 4, dense_fmaps], init=self.init, name=self.name + '_block_3', k=self.k, L=self.L[2])
+            trans_fmaps = self.fin + (L[0]+L[1]+L[2]) * k
+            trans3 = DenseTransition(input_shape=[self.batch, self.h // 4, self.w // 4, trans_fmaps], init=self.init, name=self.name + '_trans_3')
+
+            dense_fmaps = self.fin + (L[0]+L[1]+L[2]) * k
+            dense4 = DenseBlock(input_shape=[self.batch, self.h // 8, self.w // 8, dense_fmaps], init=self.init, name=self.name + '_block_4', k=self.k, L=self.L[3])
+            trans_fmaps = self.fin + (L[0]+L[1]+L[2]+L[3]) * k
+            trans4 = DenseTransition(input_shape=[self.batch, self.h // 8, self.w // 8, trans_fmaps], init=self.init, name=self.name + '_trans_4')
+
+            dense_fmaps = self.fin + + (L[0]+L[1]+L[2]+L[3]) * k
+            dense5 = DenseBlock(input_shape=[self.batch, self.h // 16, self.w // 16, dense_fmaps], init=self.init, name=self.name + '_block_5', k=self.k, L=self.L[4])
+            
+            self.blocks.append(dense1)
+            self.blocks.append(trans1)
+            self.blocks.append(dense2)
+            self.blocks.append(trans2)
+            self.blocks.append(dense3)
+            self.blocks.append(trans3)
+            self.blocks.append(dense4)
+            self.blocks.append(trans4)
+            self.blocks.append(dense5)
+            self.num_blocks = len(self.blocks)
 
     ###################################################################
 
