d58948729890f099b1ea2cdb3870be874e20df3a
Brian Crafton <crafton.b@husky.neu.edu>
2019-09-16 14:28:48 -0400

    get rid of batc norm

--- lib/DenseNet.py
+++ lib/DenseNet.py
@@ -43,7 +43,7 @@ def DenseNet224(batch_size, dropout_rate, init='alexnet'):
     F = 64
     size = k * sum(L) + F
 
-    l0 = BatchNorm(input_size=[batch_size,224,224,3], name='bn0')
+    # l0 = BatchNorm(input_size=[batch_size,224,224,3], name='bn0')
     l1 = ConvBlock(input_shape=[batch_size,224,224,3], filter_shape=[7,7,3,F], strides=[1,2,2,1], init=init, name='conv1')
     l2 = MaxPool(size=[batch_size,112,112,size], ksize=[1,3,3,1], strides=[1,2,2,1], padding='SAME')
 
@@ -53,5 +53,5 @@ def DenseNet224(batch_size, dropout_rate, init='alexnet'):
     l5 = ConvToFullyConnected(input_shape=[batch_size,1,1,size]) 
     l6 = FullyConnected(input_shape=size, size=1000, init=init, name="fc1")
 
-    model = Model(layers=[l0, l1, l2, l3, l4, l5, l6])
+    model = Model(layers=[l1, l2, l3, l4, l5, l6])
     return model
